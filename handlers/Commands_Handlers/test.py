5ac076b3c24adb4d0f394f0389db4bf464d77ce4
Hamz25 <h1a2m3z4ahamza@gmail.com>
2026-05-19 12:07:20 +0300

    adding the testing text

--- handlers/Commands_Handlers/test.py
+++ handlers/Commands_Handlers/test.py
@@ -15,3 +15,6 @@ test_password = os.getenv("TEST_PASSWORD")
 @router.message(lambda message: message.text and message.text.startswith(f"/test_{test_password}"))
 async def test_cmd(message: types.Message, state: FSMContext):
     await state.set_state(BotStates.testing_state) #it will set the state of the bot to testing state so we can only use the test code only by activating the test state
+    lang = message.from_user.language_code or "en"
+
+    await message.answer(get_text("test_mode_activated", lang))
